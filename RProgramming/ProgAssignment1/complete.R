a7442de14112a603247afc62e8e3a3e3472fbbc6
Nadja <nadja.strobbe@cern.ch>
2014-10-19 15:39:21 +0200

    made reading in files somewhat more streamlined

--- RProgramming/ProgAssignment1/complete.R
+++ RProgramming/ProgAssignment1/complete.R
@@ -1,9 +1,8 @@
 complete <- function(directory, id = 1:332) {
-  read_one <- function(id1){
-    read.csv(paste(directory,"/",sprintf("%03d",id1),".csv",sep=""))
-  }
-  list_files <- lapply(id,read_one)
-
+  # Make list of files
+  filelist <- paste0(directory, "/", sprintf("%03d",id), ".csv")
+  list_files <- lapply(filelist, read.csv)
+  
   # check for each file how many complete cases there are
   n_complete_cases <- vapply(list_files, function(x) sum(complete.cases(x)),FUN.VALUE=1)
   data.frame("id"=id, nobs=n_complete_cases)
--- RProgramming/ProgAssignment1/pollutantmean.R
+++ RProgramming/ProgAssignment1/pollutantmean.R
@@ -1,16 +1,13 @@
 pollutantmean <- function(directory, pollutant, id = 1:332) {
   # Start by reading in the required files for given id list
-  # make small function to read in one file to be passed to lapply
-  read_one <- function(id){
-    read.csv(paste(directory,"/",sprintf("%03d",id),".csv",sep=""))
-  }
-  list_files <- lapply(id,read_one)
+  filelist <- paste0(directory, "/", sprintf("%03d",id), ".csv")
+  list_files <- lapply(filelist, read.csv)
+  
   # now get info on pollutant in these files
   # first get all information combined, before calculating mean
   info_poll <- lapply(list_files,"[[",pollutant)
   # put everything in one big vector
   info_poll_vec <- unlist(info_poll)
   # compute mean, ignore NA values and output in specified precision 
-  #sprintf("%.3f",mean(info_poll_vec,na.rm=TRUE))
   mean(info_poll_vec,na.rm=TRUE)
 }
